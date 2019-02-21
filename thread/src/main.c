49d89dcdff862004d5cf78afdb4f6617dbe49af9
泥俊沛 <553166577@qq.com>
2019-02-21 11:10:09 +0800

    修改程序，去掉外层循环

--- thread/src/main.c
+++ thread/src/main.c
@@ -12,111 +12,79 @@ long AVE;
 long long *sum = NULL;
 
 double get_time();
+long long THREAD_SUM();
 void thread_sum(void* arg);
 int transToInt(char * str);
 long transToLong(char * str);
 int main() {
-    int i,j;
-    double t1,t2;
-    pthread_t *pthread_id = NULL;
-    long long result = 0;
-    long long outResult[20];
+    long long result;
     char _MAX[20];
     char _N[20];
     //目录信息
-    DIR* directory;
-    struct dirent * ptr;
-    char directBuffer[80]; // 当前目录路径
-    char fileDir[100];     // txt路径
-    char outFileDir[100];  // 输出路径
-    char outTxtDir[100];
+    char ROOT_PATH[100]        = "\0";   // 当前目录路径
+    char input_file_path[100]  = "\0";     // 输入txt路径
+    char output_file_path[100] = "\0";  // 输出路径
 
-    //获取当前路径 测试样例在当前路径/testfile/xx.txt
-    getcwd(directBuffer, sizeof(directBuffer));
-    strcat(outFileDir,directBuffer);
-    strcat(directBuffer,"/testfile/");
-    strcat(outFileDir,"/outfile/");
-    if(!(directory=opendir(directBuffer))){
-        printf("路径进入失败...");
+    // 当前路径ROOT_PATH
+    // 计算数值从ROOTPATH/input.txt中导入
+    // 输出到ROOTPATH/output.txt中
+    getcwd(ROOT_PATH, sizeof(ROOT_PATH));
+    strcat(input_file_path,ROOT_PATH);
+    strcat(input_file_path,"/input.txt");
+    strcat(output_file_path,ROOT_PATH);
+    strcat(output_file_path,"/output.txt");
+    if(!freopen(input_file_path,"r",stdin)){
+        printf("文件打开失败...");
         exit(1);
     }
-    puts(directBuffer);
-    j = 0;
-    while ((ptr=readdir(directory)) != NULL){
-        if(ptr->d_type == 8 && (strstr(ptr->d_name,".txt")!=NULL)){
-            fileDir[0] = '\0';
-            outTxtDir[0] = '\0';
-            strcat(fileDir,directBuffer);
-            strcat(fileDir,ptr->d_name);
-            strcat(outTxtDir,outFileDir);
-            strcat(outTxtDir,ptr->d_name);
-            puts(ptr->d_name);
-            puts(fileDir);
-        } else
-            continue;
-        if(!freopen(fileDir,"r",stdin)){
-            printf("文件打开失败...");
-            exit(1);
-        }
-
-        // 通过重定向输入求和上限 MAX 和线程数 N
-        scanf("%s",_N);
-        scanf("%s",_MAX);
-        fclose(stdin);
-        //得到 MAX 和 N 的值
-        MAX = transToLong(_MAX);
-        N = transToInt(_N);
-
-        //申请线程id和求和记录数组
-        pthread_id = (pthread_t*)malloc(N * sizeof(pthread_t));
-        sum = (long long*)malloc(N * sizeof(long long));
-        AVE = MAX/N;
-
-        // 计算开始时间
-        t1 = get_time();
-
-        // 创建N个子线程
-        for(i=0;i<N;i++){
-            pthread_create(pthread_id+i,NULL,thread_sum,i);
-        }
-        // 等待每一个线程结束，并把结果汇总
-        for(i=0;i<N;i++){
-            pthread_join(pthread_id[i],NULL);
-            result += sum[i];
-        }
-        // 计算结束时间
-        t2 = get_time();
-        printf("求和结果：%lld\n",result);
-        printf("求和时间：%f\n",t2 - t1);
-        puts(outTxtDir);
-        //把结果记录到输出数组
-        outResult[j] = result;
-        j++;
-        // 重置result 并释放内存
-        result = 0;
-        free(pthread_id);
-        free(sum);
-    }
-    closedir(directory);
-    if(!(directory=opendir(directBuffer))){
-        printf("路径进入失败...");
+    // 通过重定向输入求和上限 MAX 和线程数 N
+    scanf("%s",_N);
+    scanf("%s",_MAX);
+    fclose(stdin);
+    // 得到 MAX 和 N 的值,得到 AVE
+    MAX = transToLong(_MAX);
+    N = transToInt(_N);
+    AVE = MAX/N;
+    result = THREAD_SUM();
+    if(!freopen(output_file_path,"w",stdout)){
+        printf("output.txt 文件打开失败！");
         exit(1);
     }
-    j = 0;
-    while ((ptr=readdir(directory)) != NULL) {
-        if (ptr->d_type == 8 && (strstr(ptr->d_name, ".txt") != NULL)) {
-            outTxtDir[0] = '\0';
-            strcat(outTxtDir, outFileDir);
-            strcat(outTxtDir, ptr->d_name);
-        } else
-            continue;
-        // 定向输出到outfile内容
-        freopen(outTxtDir,"w",stdout);
-        printf("%lld\n",outResult[j]);
-        j++;
-    }
-    closedir(directory);
+    printf("%lld\n",result);
+    fclose(stdout);
     return 0;
+}
+long long THREAD_SUM()
+{
+    int i;
+    long AVE;
+    double t1,t2;
+    long long result = 0;
+    pthread_t *pthread_id = NULL;
+    // 申请线程id和求和记录数组
+    pthread_id = (pthread_t*)malloc(N * sizeof(pthread_t));
+    sum = (long long*)malloc(N * sizeof(long long));
+    // 计算开始时间
+    t1 = get_time();
+
+    // 创建N个子线程
+    for(i=0;i<N;i++){
+        pthread_create(pthread_id+i,NULL,thread_sum,i);
+    }
+    // 等待每一个线程结束，并把结果汇总
+    for(i=0;i<N;i++){
+        pthread_join(pthread_id[i],NULL);
+        result += sum[i];
+    }
+    // 计算结束时间
+    t2 = get_time();
+    printf("\n求和上限：%ld\n",MAX);
+    printf("线程数量：%d\n",N);
+    printf("求和结果：%lld\n",result);
+    printf("求和时间：%f\n\n",t2 - t1);
+    free(pthread_id);
+    free(sum);
+    return  result;
 }
 
 double get_time()
--- thread/test.sh
+++ thread/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./multisum
-INOUT_FILE_PATH="testfile/"
-OUTPUT_FILE_PATH="outfile/"
-RIGHT_FILE_PATH="rightfile/"
-ROOT_PATH="$(cd `dirname $0`; pwd)/"
 
+TEST_FILE_PATH="testfile/"
+ROOT_PATH="$(cd `dirname $0`; pwd)/"
+INPUT_FILE_PATH="${ROOT_PATH}/input.txt"
+OUTPUT_FILE_PATH="${ROOT_PATH}/output.txt"
+RIGHT_FILE_PATH="${ROOT_PATH}/right.txt"
 
 let success=0
 let index=0
@@ -13,17 +13,24 @@ let PASS_NUM=0
 let FAIL_NUM=0
 
 
-for file in $(ls ${ROOT_PATH}/${OUTPUT_FILE_PATH})
+for file in $(ls ${ROOT_PATH}/${TEST_FILE_PATH})
 do
     echo "${file}"
     if [[ $file =~ \.txt$ ]]
     then
-        diff ${ROOT_PATH}${OUTPUT_FILE_PATH}${file} ${ROOT_PATH}${RIGHT_FILE_PATH}${file}
+        echo "把${file}内容输出到input.txt和right.txt中..."
+        sed -n '1,2p' ${ROOT_PATH}/${TEST_FILE_PATH}${file} > ${INPUT_FILE_PATH}
+        sed -n '3,3p' ${ROOT_PATH}/${TEST_FILE_PATH}${file} > ${RIGHT_FILE_PATH}
+        echo "计算结果并输出到output.txt中..."
+        ./multisum
+        echo "计算完成..."
+        echo "开始比对output.txt和right.txt..."
+        diff "${ROOT_PATH}output.txt" "${ROOT_PATH}right.txt"
         let diff_result=$?
         if [[ ${diff_result} -eq ${success} ]];then
             let PASS_NUM+=1
         else
-           let FAIL_NUM+=1
+            let FAIL_NUM+=1
             echo "${file}测试失败"
             FAIL[${index}]="${file}"
             let index+=1
